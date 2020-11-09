d9d5b87f81965d9dcc216dbe502005a204d161e7
utkunoyan <unoyan16@ku.edu.tr>
2020-11-10 00:32:30 +0300

    Create_Plot_Scenario added

--- VLC_init.py
+++ VLC_init.py
@@ -1,15 +1,7 @@
-import numpy as np
-import time
-import os
-import subprocess
-import multiprocessing
-import resource
-import matplotlib.pyplot as plt
 import math
 from functools import lru_cache
-import scipy
-from scipy.integrate import quad, dblquad, tplquad
-from numpy import *
+import numpy as np
+from scipy.integrate import tplquad
 
 """
 *: coordinate center of cari
@@ -23,8 +15,9 @@ from numpy import *
                     d
 """
 
+
 def diff_volume(p, t, r):
-    return r ** 2 * sin(p)
+    return r ** 2 * math.sin(p)
 
 
 def calc_volume(r1, r2, t1, t2, p1, p2):
@@ -60,15 +53,16 @@ class VLC_init:
         self.aoa21 = math.atan(((self.tx2[1] - self.rx1[1]) / (self.rx1[0] - self.tx2[0])))
         self.aoa22 = math.atan(((self.tx2[1] - self.rx2[1]) / (self.rx2[0] - self.tx2[0])))
         self.aoas = np.array([[self.aoa11, self.aoa12], [self.aoa21, self.aoa22]])
-        self.eps_a, self.eps_b, self.eps_c, self.eps_d, self.phi_h = np.array([[0., 0.], [0., 0.]]), np.array([[0., 0.], [0., 0.]]), np.array([[0., 0.], [0., 0.]]), np.array([[0., 0.], [0., 0.]]), np.array([[0., 0.], [0., 0.]])
+        self.eps_a, self.eps_b, self.eps_c, self.eps_d, self.phi_h = np.array([[0., 0.], [0., 0.]]), np.array(
+            [[0., 0.], [0., 0.]]), np.array([[0., 0.], [0., 0.]]), np.array([[0., 0.], [0., 0.]]), np.array(
+            [[0., 0.], [0., 0.]])
         self.delays = np.array([[self.distancebtw11 / self.c, self.distancebtw12 / self.c],
                                 [self.distancebtw21 / self.c, self.distancebtw22 / self.c]])
         self.distances = np.array([[self.distancebtw11, self.distancebtw12], [self.distancebtw21, self.distancebtw22]])
-        self.H = np.array([[0., 0.], [0., 0.]]).astype(longfloat)
+        self.H = np.array([[0., 0.], [0., 0.]]).astype(float)
 
     @lru_cache(maxsize=None)
-    def update_cords(self, tx_cord, rx_cord, rx_radius):
-        self.rxradius = rx_radius  # mm
+    def update_cords(self, tx_cord, rx_cord):
         self.trxpos, self.trypos = (tx_cord[0][0], tx_cord[1][0]), (tx_cord[0][1], tx_cord[1][1])  # meter
         self.tx1 = np.array((self.trxpos[0], self.trypos[0]))
         self.tx2 = np.array((self.trxpos[1], self.trypos[1]))
@@ -77,21 +71,22 @@ class VLC_init:
         self.rx2 = np.array((self.rxxpos[1], self.rxypos[1]))
 
     def calculate_Hij(self, i, j):
-        txpos = np.array((self.trxpos[i-1], self.trypos[i-1]))
-        rxpos = np.array((self.rxxpos[j - 1], self.rxypos[j - 1]))
+        txpos = np.array((self.trxpos[i], self.trypos[i]))
+        rxpos = np.array((self.rxxpos[j], self.rxypos[j]))
         distance = self.distances[i][j]
         y = np.abs(txpos[1] - rxpos[1])
         x = np.abs(txpos[0] - txpos[0])
-        azimuth = math.atan(((x + self.rxradius * math.cos(self.relative_heading))/y)) - math.atan(((x - self.rxradius * math.cos(self.relative_heading))/y))
-        elevation = 2*math.atan((self.rxradius/distance))
-        return (elevation/(2*self.e_angle)) * (azimuth/(2*self.a_angle))
+        azimuth = math.atan(((x + self.rxradius * math.cos(self.relative_heading)) / y)) - math.atan(
+            ((x - self.rxradius * math.cos(self.relative_heading)) / y))
+        elevation = 2 * math.atan((self.rxradius / distance))
+        return (elevation / (2 * self.e_angle)) * (azimuth / (2 * self.a_angle))
 
     @lru_cache(maxsize=None)
     def update_lookuptable(self):
         self.calc_delay()
         self.update_aoa()
         self.update_eps()
-        self.H = np.array([[0., 0.], [0., 0.]]).astype(longfloat)
+        self.H = np.array([[0., 0.], [0., 0.]]).astype(float)
         for i in range(2):
             for j in range(2):
                 self.H[i][j] = self.calculate_Hij(i, j)
@@ -133,13 +128,12 @@ class VLC_init:
         valueScaled = float(value - leftMin) / float(leftSpan)
         # Convert the 0-1 range into a value in the right range.
         return rightMin + (valueScaled * rightSpan)
-    
+
     def change_cords(self):
         self.tx1 = np.array((self.trxpos[0], self.trypos[0]))
         self.tx2 = np.array((self.trxpos[1], self.trypos[1]))
-        tx1 = -1*(self.tx1[1]-0.5)
+        tx1 = -1 * (self.tx1[1] - 0.5)
         ty1 = self.tx1[1]
         tx2 = self.tx2[1]
-        ty2 = -1*(self.tx2[1]-0.5)
+        ty2 = -1 * (self.tx2[1] - 0.5)
         return tx1, ty1, tx2, ty2
-
