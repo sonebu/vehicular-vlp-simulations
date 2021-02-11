problem about axis is fixed, calculation of deviation is missing in, parameter_deviation_data_extractor.py

Bound_Estimation/parameter_deviation_calculator.py
@@ -389,12 +389,6 @@ def main():
             data_name = data_names[idx]
             data_dir = directory_path + '/SimulationData/' + data_name + '.mat'
             data = load_mat(data_dir)
-
-            d11 = np.sqrt(tx1[0] ^ 2 + tx1[1] ^ 2)
-            d11 = np.sqrt(tx1[0] ^ 2 + tx1[1] ^ 2)
-
-
-
             folder_name = folder_names[idx]
             dp = gen_sim_data.params.number_of_skip_data
             data_point = str(int(1000 / dp)) + '_point_' + '/'
@@ -521,20 +515,20 @@ def main():
                     [[noise_var1_soner, noise_var2_soner], [noise_var3_soner, noise_var4_soner]])
 
                 # making estimations
-                theta_l_r[itr, i] = aoa.estimate(delays=delays, H_q=H_q, noise_variance=noise_variance_soner)
-                print("AoA finished")
-                #rtof_dist[itr, i] = rtof.estimate(all_delays=delays, H=H, noise_variance=noise_variance)
-                #print("RToF finished")
-                tdoa_dist[itr, i] = tdoa.estimate(delays=delays, H=H, noise_variance=noise_variance)
-                print("TDoA finished")
+                #theta_l_r[itr, i] = aoa.estimate(delays=delays, H_q=H_q, noise_variance=noise_variance_soner)
+                #print("AoA finished")
+                rtof_dist[itr, i] = rtof.estimate(all_delays=delays, H=H, noise_variance=noise_variance)
+                print("RToF finished")
+                #tdoa_dist[itr, i] = tdoa.estimate(delays=delays, H=H, noise_variance=noise_variance)
+                #print("TDoA finished")
                 #print(time_)
     pickle_dir = directory_path + '/Bound_Estimation/Parameter_Deviation/'
-    with open(pickle_dir + 'theta.pkl', 'wb') as f:
-        pickle.dump(theta_l_r, f)
-    #with open(pickle_dir + 'rtof_dist', 'wb') as f:
-    #    pickle.dump(rtof_dist, f)
-    with open(pickle_dir + 'tdoa_dist.pkl', 'wb') as f:
-        pickle.dump(tdoa_dist, f)
+    #with open(pickle_dir + 'theta.pkl', 'wb') as f:
+    #    pickle.dump(theta_l_r, f)
+    with open(pickle_dir + 'rtof_dist', 'wb') as f:
+        pickle.dump(rtof_dist, f)
+    #with open(pickle_dir + 'tdoa_dist.pkl', 'wb') as f:
+    #   pickle.dump(tdoa_dist, f)
 
 
 if __name__ == '__main__':

Bound_Estimation/parameter_deviation_data_extractor.py
@@ -6,7 +6,7 @@ from Bound_Estimation.matfile_read import *
 from config import gen_sim_data
 
 def deviation_from_actual_value(array, actual_val):
-    return np.sqrt(np.mean(abs(array - actual_val) ** 2, axis=0))
+    return np.sqrt(np.mean(np.abs(array - actual_val) ** 2, axis=0))
 
 def main():
     directory_path = os.path.dirname(
@@ -16,27 +16,26 @@ def main():
     data = load_mat(data_dir)
     L = data['vehicle']['target']['width']
     """getting actual coordinates from .mat file"""
-    tx1_x = data['vehicle']['target_relative']['tx1_qrx4']['y'][::gen_sim_data.params.number_of_skip_data]
+    tx1_x = (-1)*data['vehicle']['target_relative']['tx1_qrx4']['y'][::gen_sim_data.params.number_of_skip_data]
     tx1_y = data['vehicle']['target_relative']['tx1_qrx4']['x'][::gen_sim_data.params.number_of_skip_data]
-    tx2_x = data['vehicle']['target_relative']['tx2_qrx3']['y'][::gen_sim_data.params.number_of_skip_data]
+    tx2_x = (-1)*data['vehicle']['target_relative']['tx2_qrx3']['y'][::gen_sim_data.params.number_of_skip_data]
     tx2_y = data['vehicle']['target_relative']['tx2_qrx3']['x'][::gen_sim_data.params.number_of_skip_data]
 
     """calculating actual theta"""
-    aoa11 = math.atan(tx1_y/tx1_x)
-    aoa12 = math.atan(tx2_y/tx2_x)
-    aoa21 = math.atan((tx1_y + L) / tx1_x)
-    aoa22 = math.atan((tx2_y + L) / tx2_x)
-    thetas = np.array([[aoa11, aoa12], [aoa21, aoa22]])
-
+    aoa11 = np.arctan(tx1_y/tx1_x)
+    aoa12 = np.arctan(tx2_y/tx2_x)
+    aoa21 = np.arctan((tx1_y - L) / tx1_x)
+    aoa22 = np.arctan((tx2_y - L) / tx2_x)
+    thetas = np.array([[aoa11, aoa12], [aoa21, aoa22]]).transpose()
     """calculating actual distance values"""
-    d11 = np.sqrt(math.pow(tx1_x, 2) + math.pow(tx1_y, 2))
-    d12 = np.sqrt(math.pow(tx2_x, 2) + math.pow(tx2_y, 2))
-    d21 = np.sqrt(math.pow(tx1_x, 2) + math.pow((tx1_y-L), 2))
-    d22 = np.sqrt(math.pow(tx1_x, 2) + math.pow((tx1_y-L), 2))
-    rtof_distances = np.array([[d11, d12], [d21, d22]])
+    d11 = np.sqrt(np.power(tx1_x, 2) + np.power(tx1_y, 2))
+    d12 = np.sqrt(np.power(tx2_x, 2) + np.power(tx2_y, 2))
+    d21 = np.sqrt(np.power(tx1_x, 2) + np.power((tx1_y-L), 2))
+    d22 = np.sqrt(np.power(tx1_x, 2) + np.power((tx1_y-L), 2))
+    rtof_distances = np.array([[d11, d12], [d21, d22]]).transpose()
     dA = d11 - d21
     dB = d12 - d22
-    tdoa_distances = np.array([dA, dB], np.newaxis)
+    tdoa_distances = np.array([dA, dB], np.newaxis).transpose()
 
     """retrieving pickle data calculated from parameter_deviation_calculator.py"""
     folder_name = gen_sim_data.names.folder_names[2]
@@ -50,14 +49,27 @@ def main():
     with open(pickle_dir + 'theta.pkl', 'rb') as f:
         theta_l_r = pickle.load(f)
         print(theta_l_r.shape)
+    """""
+    with open(pickle_dir + 'rtof_dist.pkl', 'rb') as f:
+        rtof_dist = pickle.load(f)
+        print(rtof_dist.shape)
+    """
     with open(pickle_dir + 'tdoa_dist.pkl', 'rb') as f:
         tdoa_dist = pickle.load(f)
         print(tdoa_dist.shape)
 
     """calculating deviation for theta, rtof_dist, tdoa_dist"""
-    deviation_theta = deviation_from_actual_value(theta_l_r, thetas)
-    #deviation_rtof_dist = deviation_from_actual_value(rtof_dist, rtof_distances)
-    deviation_tdoa_dist = deviation_from_actual_value(tdoa_dist, tdoa_distances)
+    deviation_theta = np.copy(theta_l_r)
+    for i in range(len(deviation_theta)):
+        deviation_theta[i] = deviation_from_actual_value(theta_l_r[i], thetas)
+    """
+    deviation_rtof_dist = np.copy(rtof_dist)
+    for i in range(len(deviation_rtof_dist)):
+        deviation_rtof_dist[i] = deviation_from_actual_value(rtof_dist[i], rtof_distances)
+    """
+    deviation_tdoa_dist = np.copy(tdoa_dist)
+    for i in range(len(deviation_tdoa_dist)):
+        deviation_tdoa_dist[i] = deviation_from_actual_value(tdoa_dist[i], tdoa_distances)
 
     """saving calculated deviation parameters."""
     with open(pickle_dir + 'deviation_theta.pkl', 'wb') as f: