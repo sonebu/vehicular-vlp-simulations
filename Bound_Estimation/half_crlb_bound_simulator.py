corrected std calculation for half crlb

Bound_Estimation/half_crlb_bound_simulator.py
@@ -13,9 +13,8 @@ def roberts_half_crlb_single_instance(crlb_inst, tx1, tx2, noise_factor):
 
     for param1, param2 in zip(range(2), range(2)):
         for i in range(2):
-            fim[param1][param2] += 1 / noise_factor[i] * crlb_inst.d_ddist_d_param(param1 + 1, i, tx1, tx2) \
+            fim[param1][param2] -= 1 / noise_factor[i] * crlb_inst.d_ddist_d_param(param1 + 1, i, tx1, tx2) \
                                   * crlb_inst.d_ddist_d_param(param2 + 1, i, tx1, tx2)
-
     return np.linalg.inv(fim)
 
 
@@ -28,7 +27,7 @@ def bechadergue_half_crlb_single_instance(crlb_inst, tx1, tx2, noise_factor):
             for j in range(2):
                 ij = (i + 1) * 10 + (j + 1)
 
-                fim[param1][param2] += 1 / noise_factor[i][j] * crlb_inst.d_dij_d_param(param1 + 1, ij, tx1, tx2) \
+                fim[param1][param2] -= 1 / noise_factor[i][j] * crlb_inst.d_dij_d_param(param1 + 1, ij, tx1, tx2) \
                                        * crlb_inst.d_dij_d_param(param2 + 1, ij, tx1, tx2)
     return np.linalg.inv(fim)
 
@@ -42,8 +41,13 @@ def soner_half_crlb_single_instance(crlb_inst, tx1, tx2, noise_factor):
             for j in range(2):
                 ij = (i + 1) * 10 + (j + 1)
 
-                fim[param1][param2] += 1 / noise_factor[i][j] * crlb_inst.d_theta_d_param(param1 + 1, ij, tx1, tx2) \
+                fim[param1][param2] -= 1 / noise_factor[i][j] * crlb_inst.d_theta_d_param(param1 + 1, ij, tx1, tx2) \
                                        * crlb_inst.d_theta_d_param(param2 + 1, ij, tx1, tx2)
+                print(crlb_inst.d_theta_d_param(param1 + 1, ij, tx1, tx2))
+                print(crlb_inst.d_theta_d_param(param2 + 1, ij, tx1, tx2))
+                print("noise", noise_factor[i][j])
+    print("max: ", np.max(fim))
+    print("min: ", np.min(fim))
     return np.linalg.inv(fim)
 
 

Bound_Estimation/parameter_deviation_data_extractor.py
@@ -10,7 +10,8 @@ def deviation_from_actual_value(array, actual_vals):
         deviations = np.zeros((array.shape[1],array.shape[2]))
         for pt in range(array.shape[1]):
             for param in range(array.shape[2]):
-                dev = np.sqrt(np.mean(np.abs(array[:,pt,param] - actual_vals[pt,param]) ** 2, axis=0))
+                #dev = np.sqrt(np.mean(np.abs(array[:,pt,param] - actual_vals[pt,param]) ** 2, axis=0))
+                dev = np.std(array[:,pt,param])
                 deviations[pt,param] = dev
         return deviations
 
@@ -19,8 +20,9 @@ def deviation_from_actual_value(array, actual_vals):
         for pt in range(array.shape[1]):
             for param_ind1 in range(array.shape[2]):
                 for param_ind2 in range(array.shape[3]):
-                    dev = np.sqrt(np.mean(np.abs(
-                        array[:, pt, param_ind1, param_ind2] - actual_vals[pt, param_ind1, param_ind2]) ** 2, axis=0))
+                    #dev = np.sqrt(np.mean(np.abs(
+                    #    array[:, pt, param_ind1, param_ind2] - actual_vals[pt, param_ind1, param_ind2]) ** 2, axis=0))
+                    dev = np.std(array[:, pt, param_ind1, param_ind2])
                     deviations[pt, param_ind1, param_ind2] = dev
         return deviations
     else:
@@ -34,9 +36,9 @@ def main():
     data = load_mat(data_dir)
     L = data['vehicle']['target']['width']
     """getting actual coordinates from .mat file"""
-    tx1_x = (-1)*data['vehicle']['target_relative']['tx1_qrx4']['y'][::gen_sim_data.params.number_of_skip_data]
+    tx1_x = data['vehicle']['target_relative']['tx1_qrx4']['y'][::gen_sim_data.params.number_of_skip_data]
     tx1_y = data['vehicle']['target_relative']['tx1_qrx4']['x'][::gen_sim_data.params.number_of_skip_data]
-    tx2_x = (-1)*data['vehicle']['target_relative']['tx2_qrx3']['y'][::gen_sim_data.params.number_of_skip_data]
+    tx2_x = data['vehicle']['target_relative']['tx2_qrx3']['y'][::gen_sim_data.params.number_of_skip_data]
     tx2_y = data['vehicle']['target_relative']['tx2_qrx3']['x'][::gen_sim_data.params.number_of_skip_data]
 
     """calculating actual theta"""
@@ -71,6 +73,10 @@ def main():
 
     with open(pickle_dir + 'theta.pkl', 'rb') as f:
         theta_l_r = pickle.load(f)
+    print(theta_l_r.shape)
+    print(theta_l_r[:,:,1,0] * 180 / math.pi)
+    print(thetas[:,1,0] * 180 / math.pi)
+    exit(0)
     """""
     with open(pickle_dir + 'rtof_dist.pkl', 'rb') as f:
         rtof_dist = pickle.load(f)