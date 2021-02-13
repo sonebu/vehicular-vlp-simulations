7d6565a996c6913f902c0d3996a61e6a76991779
mervekarakas <mkarakas16@ku.edu.tr>
2021-02-13 13:37:42 +0300

    implemented parameter deviation calculation method for multidimensional data

--- Bound_Estimation/parameter_deviation_data_extractor.py
+++ Bound_Estimation/parameter_deviation_data_extractor.py
@@ -5,8 +5,26 @@ import math
 from Bound_Estimation.matfile_read import *
 from config import gen_sim_data
 
-def deviation_from_actual_value(array, actual_val):
-    return np.sqrt(np.mean(np.abs(array - actual_val) ** 2, axis=0))
+def deviation_from_actual_value(array, actual_vals):
+    if actual_vals.ndim == 2:
+        deviations = np.zeros((array.shape[1],array.shape[2]))
+        for pt in range(array.shape[1]):
+            for param in range(array.shape[2]):
+                dev = np.sqrt(np.mean(np.abs(array[:,pt,param] - actual_vals[pt,param]) ** 2, axis=0))
+                deviations[pt,param] = dev
+        return deviations
+
+    elif actual_vals.ndim == 3:
+        deviations = np.zeros((array.shape[1], array.shape[2], array.shape[3]))
+        for pt in range(array.shape[1]):
+            for param_ind1 in range(array.shape[2]):
+                for param_ind2 in range(array.shape[3]):
+                    dev = np.sqrt(np.mean(np.abs(
+                        array[:, pt, param_ind1, param_ind2] - actual_vals[pt, param_ind1, param_ind2]) ** 2, axis=0))
+                    deviations[pt, param_ind1, param_ind2] = dev
+        return deviations
+    else:
+        raise ValueError("Wrong num of dimensions")
 
 def main():
     directory_path = os.path.dirname(
@@ -27,15 +45,19 @@ def main():
     aoa21 = np.arctan((tx1_y - L) / tx1_x)
     aoa22 = np.arctan((tx2_y - L) / tx2_x)
     thetas = np.array([[aoa11, aoa12], [aoa21, aoa22]]).transpose()
+
     """calculating actual distance values"""
     d11 = np.sqrt(np.power(tx1_x, 2) + np.power(tx1_y, 2))
     d12 = np.sqrt(np.power(tx2_x, 2) + np.power(tx2_y, 2))
     d21 = np.sqrt(np.power(tx1_x, 2) + np.power((tx1_y-L), 2))
-    d22 = np.sqrt(np.power(tx1_x, 2) + np.power((tx1_y-L), 2))
+    d22 = np.sqrt(np.power(tx2_x, 2) + np.power((tx2_y-L), 2))
     rtof_distances = np.array([[d11, d12], [d21, d22]]).transpose()
+
     dA = d11 - d21
     dB = d12 - d22
     tdoa_distances = np.array([dA, dB], np.newaxis).transpose()
+
+
 
     """retrieving pickle data calculated from parameter_deviation_calculator.py"""
     folder_name = gen_sim_data.names.folder_names[2]
@@ -46,9 +68,9 @@ def main():
     directory_path = os.path.dirname(
         os.path.dirname(os.path.join(os.getcwd(), os.listdir(os.getcwd())[0])))  ## directory of directory of file
     pickle_dir = directory_path + '/Bound_Estimation/Parameter_Deviation/'
+
     with open(pickle_dir + 'theta.pkl', 'rb') as f:
         theta_l_r = pickle.load(f)
-        print(theta_l_r.shape)
     """""
     with open(pickle_dir + 'rtof_dist.pkl', 'rb') as f:
         rtof_dist = pickle.load(f)
@@ -56,20 +78,14 @@ def main():
     """
     with open(pickle_dir + 'tdoa_dist.pkl', 'rb') as f:
         tdoa_dist = pickle.load(f)
-        print(tdoa_dist.shape)
 
     """calculating deviation for theta, rtof_dist, tdoa_dist"""
-    deviation_theta = np.copy(theta_l_r)
-    for i in range(len(deviation_theta)):
-        deviation_theta[i] = deviation_from_actual_value(theta_l_r[i], thetas)
+    deviation_theta = deviation_from_actual_value(theta_l_r, thetas)
+
     """
-    deviation_rtof_dist = np.copy(rtof_dist)
-    for i in range(len(deviation_rtof_dist)):
-        deviation_rtof_dist[i] = deviation_from_actual_value(rtof_dist[i], rtof_distances)
+    deviation_rtof_dist = deviation_from_actual_value(rtof_dist, rtof_distances)
     """
-    deviation_tdoa_dist = np.copy(tdoa_dist)
-    for i in range(len(deviation_tdoa_dist)):
-        deviation_tdoa_dist[i] = deviation_from_actual_value(tdoa_dist[i], tdoa_distances)
+    deviation_tdoa_dist = deviation_from_actual_value(tdoa_dist, tdoa_distances)
 
     """saving calculated deviation parameters."""
     with open(pickle_dir + 'deviation_theta.pkl', 'wb') as f:
