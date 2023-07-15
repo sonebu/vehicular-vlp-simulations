09f5a696564ee5a32241629d81f557b27ad549a9
sonebu <buraksoner92@gmail.com>
2023-07-15 19:32:50 +0000

    small update to setup script

--- ec2_setup.sh
+++ ec2_setup.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install -y zip
+sudo apt-get install -y zip build-essential
 wget -L https://repo.anaconda.com/miniconda/Miniconda3-py310_23.5.2-0-Linux-x86_64.sh
 bash Miniconda3-py310_23.5.2-0-Linux-x86_64.sh -b -p ~/miniconda
 rm Miniconda3-py310_23.5.2-0-Linux-x86_64.sh
