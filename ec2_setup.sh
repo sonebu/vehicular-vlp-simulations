da8845f5f1d4f38c84aead1bdb8d30773147239e
Ubuntu <ubuntu@ip-172-31-29-14.us-west-2.compute.internal>
2023-07-15 19:30:53 +0000

    setup script

---
+++ ec2_setup.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get install -y zip
+wget -L https://repo.anaconda.com/miniconda/Miniconda3-py310_23.5.2-0-Linux-x86_64.sh
+bash Miniconda3-py310_23.5.2-0-Linux-x86_64.sh -b -p ~/miniconda
+rm Miniconda3-py310_23.5.2-0-Linux-x86_64.sh
+echo 'export PATH="~/miniconda/bin:$PATH"' >> ~/.bashrc 
+source ~/.bashrc
+conda update conda -y
+conda create -p ~/venv_p310/ python=3.10 -y
+source activate ~/venv_p310/
+pip install numpy numba scipy matplotlib notebook tqdm
