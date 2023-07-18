7ffc3e3a9900cf002d5b4d94af030f8e16eeb61a
sonebu <buraksoner92@gmail.com>
2023-07-18 15:54:06 +0000

    new integrator

--- ec2_setup.sh
+++ ec2_setup.sh
@@ -3,7 +3,7 @@ sudo apt-get install -y zip build-essential
 wget -L https://repo.anaconda.com/miniconda/Miniconda3-py310_23.5.2-0-Linux-x86_64.sh
 bash Miniconda3-py310_23.5.2-0-Linux-x86_64.sh -b -p ~/miniconda
 rm Miniconda3-py310_23.5.2-0-Linux-x86_64.sh
-echo 'export PATH="~/miniconda/bin:$PATH"' >> ~/.bashrc 
+echo 'export PATH="~/miniconda/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 conda update conda -y
 conda create -p ~/venv_p310/ python=3.10 -y
